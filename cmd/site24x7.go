4050cbb0e4f47d314086712262416ec6dd264f91
tsv.titan@gmail.com <tsv.titan@gmail.com>
2024-08-26 17:57:29 +0300

    Added CreateMonitor, DeleteMonitor, PollMonitor, PollingStatus, LoggReport

--- cmd/site24x7.go
+++ cmd/site24x7.go
@@ -8,15 +8,28 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var site24x7MonitorOptions = vendors.Site24x7MonitorOptions{
+	ID: envGet("SITE24X7_MONITOR_ID", "").(string),
+}
+
 var site24x7WebsiteMonitorOptions = vendors.Site24x7WebsiteMonitorOptions{
-	Name:          envGet("SITE24X7_WEBSITE_MONITOR_NAME", "").(string),
-	URL:           envGet("SITE24X7_WEBSITE_MONITOR_URL", "").(string),
-	Method:        envGet("SITE24X7_WEBSITE_MONITOR_METHOD", "GET").(string),
-	Frequency:     envGet("SITE24X7_WEBSITE_MONITOR_FERQUENCY", "1440").(string),
-	Timeout:       envGet("SITE24X7_WEBSITE_MONITOR_TIMEOUT", 30).(int),
-	Countries:     strings.Split(envGet("SITE24X7_WEBSITE_MONITOR_COUNTRIES", "").(string), ","),
-	UserAgent:     envGet("SITE24X7_WEBSITE_MONITOR_USER_AGENT", "").(string),
-	UseNameServer: envGet("SITE24X7_WEBSITE_MONITOR_USE_NAME_SERVER", false).(bool),
+	Name:                  envGet("SITE24X7_WEBSITE_MONITOR_NAME", "").(string),
+	URL:                   envGet("SITE24X7_WEBSITE_MONITOR_URL", "").(string),
+	Method:                envGet("SITE24X7_WEBSITE_MONITOR_METHOD", "GET").(string),
+	Frequency:             envGet("SITE24X7_WEBSITE_MONITOR_FERQUENCY", "1440").(string),
+	Timeout:               envGet("SITE24X7_WEBSITE_MONITOR_TIMEOUT", 30).(int),
+	Countries:             strings.Split(envGet("SITE24X7_WEBSITE_MONITOR_COUNTRIES", "").(string), ","),
+	UserAgent:             envGet("SITE24X7_WEBSITE_MONITOR_USER_AGENT", "").(string),
+	UseNameServer:         envGet("SITE24X7_WEBSITE_MONITOR_USE_NAME_SERVER", false).(bool),
+	NotificationProfileID: envGet("SITE24X7_WEBSITE_MONITOR_USER_AGENT", "").(string),
+}
+
+var site24x7LogReportOptions = vendors.Site24x7LogReportOptions{
+	Site24x7MonitorOptions: vendors.Site24x7MonitorOptions{
+		ID: envGet("SITE24X7_MONITOR_ID", "").(string),
+	},
+	StartDate: envGet("SITE24X7_LOG_REPORT_START_DATE", "").(string),
+	EndDate:   envGet("SITE24X7_LOG_REPORT_END_DATE", "").(string),
 }
 
 var site24x7Options = vendors.Site24x7Options{
@@ -25,6 +38,7 @@ var site24x7Options = vendors.Site24x7Options{
 	ClientID:     envGet("SITE24X7_CLIENT_ID", "").(string),
 	ClientSecret: envGet("SITE24X7_CLIENT_SECRET", "").(string),
 	RefreshToken: envGet("SITE24X7_REFRESH_TOKEN", "").(string),
+	AccessToken:  envGet("SITE24X7_ACCESS_TOKEN", "").(string),
 }
 
 var site24x7Output = common.OutputOptions{
@@ -56,6 +70,7 @@ func NewSite24x7Command() *cobra.Command {
 	flags.StringVar(&site24x7Options.ClientID, "site24x7-client-id", site24x7Options.ClientID, "Site24x7 client ID")
 	flags.StringVar(&site24x7Options.ClientSecret, "site24x7-client-secret", site24x7Options.ClientSecret, "Site24x7 client secret")
 	flags.StringVar(&site24x7Options.RefreshToken, "site24x7-refresh-token", site24x7Options.RefreshToken, "Site24x7 refresh token")
+	flags.StringVar(&site24x7Options.AccessToken, "site24x7-access-token", site24x7Options.AccessToken, "Site24x7 access token")
 	flags.StringVar(&site24x7Output.Output, "site24x7-output", site24x7Output.Output, "Site24x7 output")
 	flags.StringVar(&site24x7Output.Query, "site24x7-output-query", site24x7Output.Query, "Site24x7 output query")
 
@@ -69,7 +84,7 @@ func NewSite24x7Command() *cobra.Command {
 
 			bytes, err := site24x7New(stdout).CreateWebsiteMonitor(site24x7WebsiteMonitorOptions)
 			if err != nil {
-				stdout.Error(err)
+				stdout.Error("Error: %v %s", err, string(bytes))
 				return
 			}
 			common.OutputJson(site24x7Output, "site24x7", []interface{}{site24x7Options, site24x7WebsiteMonitorOptions}, bytes, stdout)
@@ -84,7 +99,90 @@ func NewSite24x7Command() *cobra.Command {
 	flags.StringSliceVar(&site24x7WebsiteMonitorOptions.Countries, "site24x7-website-monitor-countries", site24x7WebsiteMonitorOptions.Countries, "Site24x7 website monitor countries")
 	flags.StringVar(&site24x7WebsiteMonitorOptions.UserAgent, "site24x7-website-monitor-user-agent", site24x7WebsiteMonitorOptions.UserAgent, "Site24x7 website monitor user agent")
 	flags.BoolVar(&site24x7WebsiteMonitorOptions.UseNameServer, "site24x7-website-monitor-use-name-server", site24x7WebsiteMonitorOptions.UseNameServer, "Site24x7 website monitor use name server")
+	flags.StringSliceVar(&site24x7WebsiteMonitorOptions.UserGroupIDs, "site24x7-website-monitor-user-group-ids", site24x7WebsiteMonitorOptions.UserGroupIDs, "Site24x7 website monitor user group ids")
 	site24x7Cmd.AddCommand(site24x7CreateMonitorCmd)
+
+	site24x7DeleteMonitorCmd := &cobra.Command{
+		Use:   "delete-monitor",
+		Short: "Delete montitor",
+		Run: func(cmd *cobra.Command, args []string) {
+
+			stdout.Debug("Deleting monitor...")
+			common.Debug("Site24x7", site24x7MonitorOptions, stdout)
+
+			bytes, err := site24x7New(stdout).DeleteMonitor(site24x7MonitorOptions)
+			if err != nil {
+				stdout.Error("Error: %v %s", err, string(bytes))
+				return
+			}
+			common.OutputJson(site24x7Output, "site24x7", []interface{}{site24x7Options, site24x7MonitorOptions}, bytes, stdout)
+		},
+	}
+	flags = site24x7DeleteMonitorCmd.PersistentFlags()
+	flags.StringVar(&site24x7MonitorOptions.ID, "site24x7-monitor-id", site24x7MonitorOptions.ID, "Site24x7 monitor id")
+	site24x7Cmd.AddCommand(site24x7DeleteMonitorCmd)
+
+	site24x7PollMonitorCmd := &cobra.Command{
+		Use:   "poll-monitor",
+		Short: "Poll montitor",
+		Run: func(cmd *cobra.Command, args []string) {
+
+			stdout.Debug("Polling monitor...")
+			common.Debug("Site24x7", site24x7MonitorOptions, stdout)
+
+			bytes, err := site24x7New(stdout).PollMonitor(site24x7MonitorOptions)
+			if err != nil {
+				stdout.Error("Error: %v %s", err, string(bytes))
+				return
+			}
+			common.OutputJson(site24x7Output, "site24x7", []interface{}{site24x7Options, site24x7MonitorOptions}, bytes, stdout)
+		},
+	}
+	flags = site24x7PollMonitorCmd.PersistentFlags()
+	flags.StringVar(&site24x7MonitorOptions.ID, "site24x7-monitor-id", site24x7MonitorOptions.ID, "Site24x7 monitor id")
+	site24x7Cmd.AddCommand(site24x7PollMonitorCmd)
+
+	site24x7PollingStatusCmd := &cobra.Command{
+		Use:   "polling-status",
+		Short: "Polling status",
+		Run: func(cmd *cobra.Command, args []string) {
+
+			stdout.Debug("Polling status...")
+			common.Debug("Site24x7", site24x7MonitorOptions, stdout)
+
+			bytes, err := site24x7New(stdout).PollingStatus(site24x7MonitorOptions)
+			if err != nil {
+				stdout.Error("Error: %v %s", err, string(bytes))
+				return
+			}
+			common.OutputJson(site24x7Output, "site24x7", []interface{}{site24x7Options, site24x7MonitorOptions}, bytes, stdout)
+		},
+	}
+	flags = site24x7PollingStatusCmd.PersistentFlags()
+	flags.StringVar(&site24x7MonitorOptions.ID, "site24x7-monitor-id", site24x7MonitorOptions.ID, "Site24x7 monitor id")
+	site24x7Cmd.AddCommand(site24x7PollingStatusCmd)
+
+	site24x7LogReportCmd := &cobra.Command{
+		Use:   "log-report",
+		Short: "Logging report",
+		Run: func(cmd *cobra.Command, args []string) {
+
+			stdout.Debug("Logging report...")
+			common.Debug("Site24x7", site24x7LogReportOptions, stdout)
+
+			bytes, err := site24x7New(stdout).LogReport(site24x7LogReportOptions)
+			if err != nil {
+				stdout.Error("Error: %v %s", err, string(bytes))
+				return
+			}
+			common.OutputJson(site24x7Output, "site24x7", []interface{}{site24x7Options, site24x7LogReportOptions}, bytes, stdout)
+		},
+	}
+	flags = site24x7LogReportCmd.PersistentFlags()
+	flags.StringVar(&site24x7LogReportOptions.ID, "site24x7-monitor-id", site24x7LogReportOptions.ID, "Site24x7 monitor id")
+	flags.StringVar(&site24x7LogReportOptions.StartDate, "site24x7-log-report-start-date", site24x7LogReportOptions.StartDate, "Site24x7 log report start date")
+	flags.StringVar(&site24x7LogReportOptions.EndDate, "site24x7-log-report-end-date", site24x7LogReportOptions.EndDate, "Site24x7 log report end date")
+	site24x7Cmd.AddCommand(site24x7LogReportCmd)
 
 	return site24x7Cmd
 }
--- vendors/site24x7.go
+++ vendors/site24x7.go
@@ -1,26 +1,39 @@
 package vendors
 
 import (
-	"context"
+	"bytes"
 	"encoding/json"
+	"fmt"
+	"mime/multipart"
 	"net/http"
 	"net/url"
 	"path"
 
 	"github.com/devopsext/tools/common"
 	"github.com/devopsext/utils"
-	"golang.org/x/oauth2"
 )
 
+type Site24x7MonitorOptions struct {
+	ID string
+}
+
 type Site24x7WebsiteMonitorOptions struct {
-	Name          string
-	URL           string
-	Method        string
-	Frequency     string
-	Timeout       int
-	Countries     []string
-	UserAgent     string
-	UseNameServer bool
+	Name                  string
+	URL                   string
+	Method                string
+	Frequency             string
+	Timeout               int
+	Countries             []string
+	UserAgent             string
+	UseNameServer         bool
+	NotificationProfileID string
+	UserGroupIDs          []string
+}
+
+type Site24x7LogReportOptions struct {
+	Site24x7MonitorOptions
+	StartDate string
+	EndDate   string
 }
 
 type Site24x7WebsiteMonitor struct {
@@ -40,14 +53,60 @@ type Site24x7WebsiteMonitor struct {
 	UseNameServer         bool     `json:"use_name_server,omitempty"`
 }
 
+type Site24x7AuthReponse struct {
+	AccessToken string `json:"access_token"`
+	ApiDomain   string `json:"api_domain"`
+	TokenType   string `json:"token_type"`
+	ExpiresIn   int    `json:"expires_in"`
+}
+
+type Site24x7Reponse struct {
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+}
+
+type Site24x7PollingStatusData struct {
+	Status     string `json:"status"`
+	MonmitorID string `json:"monitor_id"`
+}
+
+type Site24x7PollingStatusReponse struct {
+	Site24x7Reponse
+	Data *Site24x7PollingStatusData `json:"data,omitempty"`
+}
+
+type Site24x7LogReportDataReport struct {
+	ConnectionTime     string `json:"connection_time"`
+	DnsTime            string `json:"dns_time"`
+	SSLTime            string `json:"ssl_time"`
+	ResponseCode       string `json:"response_code"`
+	CollectionTime     string `json:"collection_time"`
+	Availability       string `json:"availability"`
+	ResponseTime       string `json:"response_time"`
+	LocationID         string `json:"location_id"`
+	Nameserver         string `json:"nameserver"`
+	ResolvedIP         string `json:"resolved_ip"`
+	Reason             string `json:"reason"`
+	ContentLength      string `json:"content_length"`
+	DataCollectionType string `json:"data_collection_type"`
+}
+
+type Site24x7LogReportData struct {
+	Report []*Site24x7LogReportDataReport
+}
+
+type Site24x7LogReportReponse struct {
+	Site24x7Reponse
+	Data *Site24x7LogReportData `json:"data,omitempty"`
+}
+
 type Site24x7Options struct {
-	Timeout               int
-	Insecure              bool
-	ClientID              string
-	ClientSecret          string
-	RefreshToken          string
-	NotificationProfileID string
-	UserGroupIDs          []string
+	Timeout      int
+	Insecure     bool
+	ClientID     string
+	ClientSecret string
+	RefreshToken string
+	AccessToken  string
 }
 
 type Site24x7 struct {
@@ -57,19 +116,101 @@ type Site24x7 struct {
 }
 
 const (
-	zohoOAuthV2TokenURL = "https://accounts.zoho.com/oauth/v2/token"
-	site24x7ApiURL      = "https://www.site24x7.com/api"
-	site24x7Monitors    = "/monitors"
-	site24x7ContentType = "application/json"
+	ZohoOAuthV2TokenURL          = "https://accounts.zoho.com/oauth/v2/token"
+	Site24x7ApiURL               = "https://www.site24x7.com/api"
+	Site24x7Monitors             = "/monitors"
+	Site24x7MonitorPollNow       = "/monitor/poll_now"
+	Site24x7MonitorStatusPollNow = "/monitor/status_poll_now"
+	Site24x7LogReports           = "/reports/log_reports"
+	Site24x7ContentType          = "application/json"
 )
 
-func (s *Site24x7) CustomCreateWebsiteMonitor(site24x7Options Site24x7Options, createMonitorOptions Site24x7WebsiteMonitorOptions) ([]byte, error) {
+// go to https://api-console.zoho.com/ and generate code with Site24x7.Admin.All scope
+// do curl with params according https://www.site24x7.com/help/api/#authentication page
+// copy refresh token and put into the options
 
-	u, err := url.Parse(site24x7ApiURL)
+func (s *Site24x7) getTokenAuth(opts Site24x7Options) (*Site24x7AuthReponse, error) {
+
+	var body bytes.Buffer
+	w := multipart.NewWriter(&body)
+	defer func() {
+		w.Close()
+	}()
+
+	if !utils.IsEmpty(opts.ClientID) {
+		if err := w.WriteField("client_id", opts.ClientID); err != nil {
+			return nil, err
+		}
+	}
+	if !utils.IsEmpty(opts.ClientSecret) {
+		if err := w.WriteField("client_secret", opts.ClientSecret); err != nil {
+			return nil, err
+		}
+	}
+	if !utils.IsEmpty(opts.RefreshToken) {
+		if err := w.WriteField("refresh_token", opts.RefreshToken); err != nil {
+			return nil, err
+		}
+	}
+	if err := w.WriteField("grant_type", "refresh_token"); err != nil {
+		return nil, err
+	}
+
+	if err := w.Close(); err != nil {
+		return nil, err
+	}
+
+	u, err := url.Parse(ZohoOAuthV2TokenURL)
 	if err != nil {
 		return nil, err
 	}
-	u.Path = path.Join(u.Path, site24x7Monitors)
+
+	bytes, err := utils.HttpPostRaw(s.client, u.String(), w.FormDataContentType(), "", body.Bytes())
+	if err != nil {
+		return nil, err
+	}
+
+	var r Site24x7AuthReponse
+	err = json.Unmarshal(bytes, &r)
+	if err != nil {
+		return nil, err
+	}
+	return &r, nil
+}
+
+func (s *Site24x7) getAuth(token string) string {
+	return fmt.Sprintf("Zoho-oauthtoken %s", token)
+}
+
+func (s *Site24x7) getAccessToken(opts Site24x7Options) (string, error) {
+
+	r, err := s.getTokenAuth(opts)
+	if err != nil {
+		return "", err
+	}
+	return r.AccessToken, nil
+}
+
+func (s *Site24x7) CustomGetAccessToken(opts Site24x7Options) (string, error) {
+
+	if !utils.IsEmpty(opts.AccessToken) {
+		return opts.AccessToken, nil
+	}
+	return s.getAccessToken(opts)
+}
+
+func (s *Site24x7) CustomCreateWebsiteMonitor(site24x7Options Site24x7Options, createMonitorOptions Site24x7WebsiteMonitorOptions) ([]byte, error) {
+
+	at, err := s.CustomGetAccessToken(site24x7Options)
+	if err != nil {
+		return nil, err
+	}
+
+	u, err := url.Parse(Site24x7ApiURL)
+	if err != nil {
+		return nil, err
+	}
+	u.Path = path.Join(u.Path, Site24x7Monitors)
 
 	r := &Site24x7WebsiteMonitor{
 		DisplayName:           createMonitorOptions.Name,
@@ -77,10 +218,10 @@ func (s *Site24x7) CustomCreateWebsiteMonitor(site24x7Options Site24x7Options, c
 		Website:               createMonitorOptions.URL,
 		CheckFrequency:        createMonitorOptions.Frequency,
 		Timeout:               createMonitorOptions.Timeout,
-		LocationProfileID:     "",
-		NotificationProfileID: site24x7Options.NotificationProfileID,
-		ThresholdProfileID:    "",
-		UserGroupIDs:          site24x7Options.UserGroupIDs,
+		LocationProfileID:     "195603000111268001",
+		NotificationProfileID: "195603000111268003",
+		ThresholdProfileID:    "195603000111268005",
+		UserGroupIDs:          createMonitorOptions.UserGroupIDs,
 		HttpMethod:            createMonitorOptions.Method,
 		UserAgent:             createMonitorOptions.UserAgent,
 	}
@@ -90,35 +231,109 @@ func (s *Site24x7) CustomCreateWebsiteMonitor(site24x7Options Site24x7Options, c
 		return nil, err
 	}
 
-	return utils.HttpPostRaw(s.client, u.String(), site24x7ContentType, "", req)
+	return utils.HttpPostRaw(s.client, u.String(), Site24x7ContentType, s.getAuth(at), req)
 }
 
 func (s *Site24x7) CreateWebsiteMonitor(options Site24x7WebsiteMonitorOptions) ([]byte, error) {
 	return s.CustomCreateWebsiteMonitor(s.options, options)
 }
 
+func (s *Site24x7) CustomDeleteMonitor(site24x7Options Site24x7Options, deleteMonitorOptions Site24x7MonitorOptions) ([]byte, error) {
+
+	at, err := s.CustomGetAccessToken(site24x7Options)
+	if err != nil {
+		return nil, err
+	}
+
+	u, err := url.Parse(Site24x7ApiURL)
+	if err != nil {
+		return nil, err
+	}
+	u.Path = path.Join(u.Path, Site24x7Monitors, deleteMonitorOptions.ID)
+
+	return utils.HttpDeleteRaw(s.client, u.String(), Site24x7ContentType, s.getAuth(at), nil)
+}
+
+func (s *Site24x7) DeleteMonitor(options Site24x7MonitorOptions) ([]byte, error) {
+	return s.CustomDeleteMonitor(s.options, options)
+}
+
+func (s *Site24x7) CustomPollMonitor(site24x7Options Site24x7Options, pollMonitorOptions Site24x7MonitorOptions) ([]byte, error) {
+
+	at, err := s.CustomGetAccessToken(site24x7Options)
+	if err != nil {
+		return nil, err
+	}
+
+	u, err := url.Parse(Site24x7ApiURL)
+	if err != nil {
+		return nil, err
+	}
+	u.Path = path.Join(u.Path, Site24x7MonitorPollNow, pollMonitorOptions.ID)
+
+	return utils.HttpGetRaw(s.client, u.String(), Site24x7ContentType, s.getAuth(at))
+}
+
+func (s *Site24x7) PollMonitor(options Site24x7MonitorOptions) ([]byte, error) {
+	return s.CustomPollMonitor(s.options, options)
+}
+
+func (s *Site24x7) CustomPollingStatus(site24x7Options Site24x7Options, pollMonitorOptions Site24x7MonitorOptions) ([]byte, error) {
+
+	at, err := s.CustomGetAccessToken(site24x7Options)
+	if err != nil {
+		return nil, err
+	}
+
+	u, err := url.Parse(Site24x7ApiURL)
+	if err != nil {
+		return nil, err
+	}
+	u.Path = path.Join(u.Path, Site24x7MonitorStatusPollNow, pollMonitorOptions.ID)
+
+	return utils.HttpGetRaw(s.client, u.String(), Site24x7ContentType, s.getAuth(at))
+}
+
+func (s *Site24x7) PollingStatus(options Site24x7MonitorOptions) ([]byte, error) {
+	return s.CustomPollingStatus(s.options, options)
+}
+
+func (s *Site24x7) CustomLogReport(site24x7Options Site24x7Options, logReportOptions Site24x7LogReportOptions) ([]byte, error) {
+
+	at, err := s.CustomGetAccessToken(site24x7Options)
+	if err != nil {
+		return nil, err
+	}
+
+	u, err := url.Parse(Site24x7ApiURL)
+	if err != nil {
+		return nil, err
+	}
+
+	var params = make(url.Values)
+
+	if !utils.IsEmpty(logReportOptions.StartDate) && !utils.IsEmpty(logReportOptions.EndDate) {
+		params.Add("start_date", logReportOptions.StartDate)
+		params.Add("end_date", logReportOptions.EndDate)
+	}
+
+	if !utils.IsEmpty(logReportOptions.StartDate) && utils.IsEmpty(logReportOptions.EndDate) {
+		params.Add("date", logReportOptions.StartDate)
+	}
+
+	u.Path = path.Join(u.Path, Site24x7LogReports, logReportOptions.ID)
+	u.RawQuery = params.Encode()
+	return utils.HttpGetRaw(s.client, u.String(), Site24x7ContentType, s.getAuth(at))
+}
+
+func (s *Site24x7) LogReport(options Site24x7LogReportOptions) ([]byte, error) {
+	return s.CustomLogReport(s.options, options)
+}
+
 func NewSite24x7(options Site24x7Options, logger common.Logger) *Site24x7 {
 
-	config := &oauth2.Config{
-		ClientID:     options.ClientID,
-		ClientSecret: options.ClientSecret,
-
-		Endpoint: oauth2.Endpoint{
-			TokenURL:  zohoOAuthV2TokenURL,
-			AuthStyle: oauth2.AuthStyleInParams,
-		},
-	}
-
-	t := &oauth2.Token{
-		RefreshToken: options.RefreshToken,
-		TokenType:    "Zoho-oauthtoken",
-	}
-
-	client := utils.NewHttpClient(options.Timeout, options.Insecure)
-	ctx := context.WithValue(context.Background(), oauth2.HTTPClient, client)
-
 	return &Site24x7{
-		client:  config.Client(ctx, t),
+		client:  utils.NewHttpClient(options.Timeout, options.Insecure),
 		options: options,
 		logger:  logger,
 	}
