587f1400515ab01ab22c5fb4a4a32ce7c6a3a138
denis.shaparenko <denis.shaparenko@exness.com>
2024-09-04 12:20:08 +0300

    add vendor linux, add vcenter actions reset/start/stop/status

--- vendors/vcenter.go
+++ vendors/vcenter.go
@@ -33,6 +33,20 @@ type VCenterOptions struct {
 	Session  string
 }
 
+type VCenterVMNameOptions struct {
+	Names []string
+}
+
+type VMInfo struct {
+	MemorySizeMiB int    `json:"memory_size_MiB"`
+	VM            string `json:"vm"`
+	Name          string `json:"name"`
+	PowerState    string `json:"power_state"`
+	CPUCount      int    `json:"cpu_count"`
+}
+type VMsResponse struct {
+	Value []VMInfo `json:"value"`
+}
 type VCenter struct {
 	client  *http.Client
 	options VCenterOptions
@@ -49,12 +63,10 @@ const (
 	VCenterRestHostPath               = "/rest/vcenter/host"
 	VCenterRestVMPath                 = "/rest/vcenter/vm"
 	VCenterRestVMGuestIdentityPathFmt = "/rest/vcenter/vm/%s/guest/identity"
+	VCenterRestVMPowerPathFmt         = "/rest/vcenter/vm/%s/power"
 )
 
-// https://developer.vmware.com/apis/vsphere-automation/latest/vcenter/vm/
-
 func (vc *VCenter) getAuth(opts VCenterOptions) string {
-
 	auth := ""
 	if !utils.IsEmpty(opts.User) {
 		userPass := fmt.Sprintf("%s:%s", opts.User, opts.Password)
@@ -65,7 +77,6 @@ func (vc *VCenter) getAuth(opts VCenterOptions) string {
 }
 
 func (vc *VCenter) getSession(opts VCenterOptions) (string, error) {
-
 	u, err := url.Parse(opts.URL)
 	if err != nil {
 		return "", err
@@ -87,7 +98,6 @@ func (vc *VCenter) getSession(opts VCenterOptions) (string, error) {
 }
 
 func (vc *VCenter) getHeaders(session string) map[string]string {
-
 	headers := make(map[string]string)
 	headers["Content-type"] = VCenterContentType
 	headers["vmware-api-session-id"] = session
@@ -95,7 +105,6 @@ func (vc *VCenter) getHeaders(session string) map[string]string {
 }
 
 func (vc *VCenter) CustomGetSession(options VCenterOptions) (string, error) {
-
 	if utils.IsEmpty(options.Session) {
 		s, err := vc.getSession(options)
 		if err != nil {
@@ -107,7 +116,6 @@ func (vc *VCenter) CustomGetSession(options VCenterOptions) (string, error) {
 }
 
 func (vc *VCenter) CustomGetClusters(options VCenterOptions) ([]byte, error) {
-
 	session, err := vc.CustomGetSession(options)
 	if err != nil {
 		return nil, err
@@ -127,7 +135,6 @@ func (vc *VCenter) GetClusters() ([]byte, error) {
 }
 
 func (vc *VCenter) CustomGetHosts(options VCenterOptions, hostOptions VCenterHostOptions) ([]byte, error) {
-
 	session, err := vc.CustomGetSession(options)
 	if err != nil {
 		return nil, err
@@ -154,7 +161,6 @@ func (vc *VCenter) GetHosts(options VCenterHostOptions) ([]byte, error) {
 }
 
 func (vc *VCenter) CustomGetVMs(options VCenterOptions, vmOptions VCenterVMOptions) ([]byte, error) {
-
 	session, err := vc.CustomGetSession(options)
 	if err != nil {
 		return nil, err
@@ -182,7 +188,6 @@ func (vc *VCenter) GetVMs(options VCenterVMOptions) ([]byte, error) {
 }
 
 func (vc *VCenter) CustomGetVMGuestIdentity(options VCenterOptions, vmGuestidentity VCenterVMGuestIdentityOptions) ([]byte, error) {
-
 	session, err := vc.CustomGetSession(options)
 	if err != nil {
 		return nil, err
@@ -202,10 +207,106 @@ func (vc *VCenter) GetVMGuestIdentity(options VCenterVMGuestIdentityOptions) ([]
 	return vc.CustomGetVMGuestIdentity(vc.options, options)
 }
 
+func (vc *VCenter) GetVMsByName(options VCenterVMNameOptions) ([]byte, error) {
+
+	return vc.CustomGetVMsByName(vc.options, options)
+}
+
+func (vc *VCenter) CustomGetVMsByName(options VCenterOptions, vmNameOptions VCenterVMNameOptions) ([]byte, error) {
+	session, err := vc.CustomGetSession(options)
+	if err != nil {
+		return nil, err
+	}
+
+	u, err := url.Parse(options.URL)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(vmNameOptions.Names) > 0 {
+		var params = make(url.Values)
+		for _, name := range vmNameOptions.Names {
+			params.Add("filter.names", name)
+		}
+		u.RawQuery = params.Encode()
+	}
+
+	u.Path = path.Join(u.Path, VCenterRestVMPath)
+
+	return utils.HttpGetRawWithHeaders(vc.client, u.String(), vc.getHeaders(session))
+}
+
+func (vc *VCenter) CustomControlVMPower(options VCenterOptions, vmID string, action string) ([]byte, error) {
+	session, err := vc.CustomGetSession(options)
+	if err != nil {
+		return nil, err
+	}
+
+	u, err := url.Parse(options.URL)
+	if err != nil {
+		return nil, err
+	}
+
+	p := fmt.Sprintf(VCenterRestVMPowerPathFmt, vmID)
+	u.Path = path.Join(u.Path, fmt.Sprintf("%s/%s", p, action))
+
+	return utils.HttpPostRawWithHeaders(vc.client, u.String(), vc.getHeaders(session), nil)
+}
+
+func (vc *VCenter) CustomGetVM(options VCenterOptions, vmID string) ([]byte, error) {
+	session, err := vc.CustomGetSession(options)
+	if err != nil {
+		return nil, err
+	}
+
+	u, err := url.Parse(options.URL)
+	if err != nil {
+		return nil, err
+	}
+
+	u.Path = path.Join(u.Path, fmt.Sprintf("%s/%s", VCenterRestVMPath, vmID))
+
+	return utils.HttpGetRawWithHeaders(vc.client, u.String(), vc.getHeaders(session))
+}
+
+func (vc *VCenter) StartVM(vmID string) ([]byte, error) {
+	return vc.CustomControlVMPower(vc.options, vmID, "start")
+}
+
+func (vc *VCenter) StopVM(vmID string) ([]byte, error) {
+	return vc.CustomControlVMPower(vc.options, vmID, "stop")
+}
+
+func (vc *VCenter) RestartVM(vmID string) ([]byte, error) {
+	return vc.CustomControlVMPower(vc.options, vmID, "reset")
+}
+
+func (vc *VCenter) GetVM(vmID string) ([]byte, error) {
+	return vc.CustomGetVM(vc.options, vmID)
+}
+
 func NewVCenter(options VCenterOptions) *VCenter {
 
 	return &VCenter{
 		client:  utils.NewHttpClient(options.Timeout, options.Insecure),
 		options: options,
 	}
+}
+
+func InitializeVCenterSession(options VCenterOptions) (VCenterOptions, error) {
+	client := utils.NewHttpClient(options.Timeout, options.Insecure)
+
+	tempVC := &VCenter{
+		client:  client,
+		options: options,
+	}
+
+	session, err := tempVC.CustomGetSession(options)
+	if err != nil {
+
+		return options, err
+	}
+
+	options.Session = session
+	return options, nil
 }
