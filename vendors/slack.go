f45962eead4a12193b578c7eb5cc3f50fcf15158
Vladislav Radin <vladislav.radin@exness.com>
2025-06-18 12:30:36 +0300

    upd

--- vendors/slack.go
+++ vendors/slack.go
@@ -90,6 +90,21 @@ type GetConversationHistoryParameters struct {
 	IncludeAllMetadata bool
 }
 
+type GetConversationHistoryResponse struct {
+	Ok       bool `json:"ok"`
+	Messages []struct {
+		Type string `json:"type"`
+		User string `json:"user"`
+		Text string `json:"text"`
+		Ts   string `json:"ts"`
+	} `json:"messages"`
+	HasMore          bool `json:"has_more"`
+	PinCount         int  `json:"pin_count"`
+	ResponseMetadata struct {
+		NextCursor string `json:"next_cursor"`
+	} `json:"response_metadata"`
+}
+
 type SlackOutputOptions struct {
 	Output      string // path to output if empty to stdout
 	OutputQuery string
