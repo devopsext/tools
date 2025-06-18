12aa74a20a30b7abe17c5c586a70badd5a6021cf
Vladislav Radin <vladislav.radin@exness.com>
2025-06-18 15:06:12 +0300

    upd

--- vendors/slack.go
+++ vendors/slack.go
@@ -581,6 +581,10 @@ func (s *Slack) CustomGetConversationHistory(slackOptions SlackOptions, getConve
 		}
 	}
 
+	if err := w.Close(); err != nil {
+		return nil, err
+	}
+
 	return utils.HttpPostRaw(s.client, s.apiURL(slackConversationsHistory), w.FormDataContentType(), s.getAuth(slackOptions), body.Bytes())
 
 }
