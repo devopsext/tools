b352180962bcf206d565adcf88dbec66d933efcd
tsv.titan@gmail.com <tsv.titan@gmail.com>
2025-07-30 16:55:54 +0300

    Added k8s vendor, deleting & scaling resources

--- cmd/k8s.go
+++ cmd/k8s.go
@@ -6,9 +6,16 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var k8sPodOptions = vendors.K8sPodOptions{
-	Namespace: envGet("K8S_POD_NAMESPACE", "").(string),
-	Name:      envGet("K8S_POD_NAME", "").(string),
+var k8sResourceOptions = vendors.K8sResourceOptions{
+	Kind:      envGet("K8S_RESOURCE_KIND", "").(string),
+	Namespace: envGet("K8S_RESOURCE_NAMESPACE", "").(string),
+	Name:      envGet("K8S_RESOURCE_NAME", "").(string),
+}
+
+var k8sResourceDeleteOptions = vendors.K8sResourceDeleteOptions{}
+
+var k8sResourceScaleOptions = vendors.K8sResourceScaleOptions{
+	Replicas: envGet("K8S_RESOURCE_REPLICAS", -1).(int),
 }
 
 var k8sOptions = vendors.K8sOptions{
@@ -35,35 +42,61 @@ func NewK8sCommand() *cobra.Command {
 	}
 	flags := k8sCmd.PersistentFlags()
 	flags.StringVar(&k8sOptions.Config, "k8s-config", k8sOptions.Config, "K8s config")
+	flags.IntVar(&k8sOptions.Timeout, "k8s-timeout", k8sOptions.Timeout, "K8s timeout")
 	flags.StringVar(&k8sOutput.Output, "k8s-output", k8sOutput.Output, "K8s output")
 	flags.StringVar(&k8sOutput.Query, "k8s-output-query", k8sOutput.Query, "K8s output query")
 
-	podCmd := &cobra.Command{
-		Use:   "pod",
-		Short: "K8s Pod tools",
+	resourceCmd := &cobra.Command{
+		Use:   "resource",
+		Short: "K8s Resource tools",
 	}
-	flags = podCmd.PersistentFlags()
-	flags.StringVar(&k8sPodOptions.Namespace, "k8s-namespace", k8sPodOptions.Namespace, "K8s Pod namespace")
-	flags.StringVar(&k8sPodOptions.Name, "k8s-name", k8sPodOptions.Name, "K8s Pod name")
-	k8sCmd.AddCommand(podCmd)
+	flags = resourceCmd.PersistentFlags()
+	flags.StringVar(&k8sResourceOptions.Kind, "k8s-resource-kind", k8sResourceOptions.Kind, "K8s Resource kind")
+	flags.StringVar(&k8sResourceOptions.Namespace, "k8s-resource-namespace", k8sResourceOptions.Namespace, "K8s Resource namespace")
+	flags.StringVar(&k8sResourceOptions.Name, "k8s-resource-name", k8sResourceOptions.Name, "K8s Resource name")
+	k8sCmd.AddCommand(resourceCmd)
 
-	podDeleteCmd := &cobra.Command{
+	resourceDeleteCmd := &cobra.Command{
 		Use:   "delete",
-		Short: "K8s Pod delete",
+		Short: "K8s Resource delete",
 		Run: func(cmd *cobra.Command, args []string) {
 
-			stdout.Debug("K8s pod deleting...")
-			common.Debug("K8s", k8sPodOptions, stdout)
+			stdout.Debug("K8s resource deleting...")
 
-			bytes, err := k8sNew(stdout).PodDelete(k8sPodOptions)
+			k8sResourceDeleteOptions.K8sResourceOptions = k8sResourceOptions
+			common.Debug("K8s", k8sResourceDeleteOptions, stdout)
+
+			bytes, err := k8sNew(stdout).ResourceDelete(k8sResourceDeleteOptions)
 			if err != nil {
 				stdout.Error(err)
 				return
 			}
-			common.OutputJson(k8sOutput, "K8s", []interface{}{k8sOptions, k8sPodOptions}, bytes, stdout)
+			common.OutputJson(k8sOutput, "K8s", []interface{}{k8sOptions, k8sResourceDeleteOptions}, bytes, stdout)
 		},
 	}
-	podCmd.AddCommand(podDeleteCmd)
+	resourceCmd.AddCommand(resourceDeleteCmd)
+
+	resourceScaleCmd := &cobra.Command{
+		Use:   "scale",
+		Short: "K8s Resource scale",
+		Run: func(cmd *cobra.Command, args []string) {
+
+			stdout.Debug("K8s resource scaling...")
+
+			k8sResourceScaleOptions.K8sResourceOptions = k8sResourceOptions
+			common.Debug("K8s", k8sResourceScaleOptions, stdout)
+
+			bytes, err := k8sNew(stdout).ResourceScale(k8sResourceScaleOptions)
+			if err != nil {
+				stdout.Error(err)
+				return
+			}
+			common.OutputJson(k8sOutput, "K8s", []interface{}{k8sOptions, k8sResourceScaleOptions}, bytes, stdout)
+		},
+	}
+	flags = resourceScaleCmd.PersistentFlags()
+	flags.IntVar(&k8sResourceScaleOptions.Replicas, "k8s-resource-replicas", k8sResourceScaleOptions.Replicas, "K8s Resource replicas")
+	resourceCmd.AddCommand(resourceScaleCmd)
 
 	return k8sCmd
 }
--- vendors/k8s.go
+++ vendors/k8s.go
@@ -2,17 +2,32 @@ package vendors
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	"github.com/devopsext/tools/common"
+	"github.com/devopsext/utils"
+	appsv1 "k8s.io/api/apps/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 )
 
-type K8sPodOptions struct {
+type K8sResourceOptions struct {
+	Kind      string
 	Namespace string
 	Name      string
+}
+
+type K8sResourceDeleteOptions struct {
+	K8sResourceOptions
+}
+
+type K8sResourceScaleOptions struct {
+	K8sResourceOptions
+	Replicas int
 }
 
 type K8sOptions struct {
@@ -25,6 +40,14 @@ type K8s struct {
 	logger    common.Logger
 	clientset *kubernetes.Clientset
 }
+
+const (
+	K8sResourcePods         = "pods"
+	K8sResourceDeployments  = "deployments"
+	K8sResourceReplicaSets  = "replicasets"
+	K8sResourceStatefulSets = "statefulsets"
+	K8sResourceDaemonSets   = "daemonsets"
+)
 
 func newK8sClient(options K8sOptions) (*kubernetes.Clientset, error) {
 
@@ -40,36 +63,145 @@ func newK8sClient(options K8sOptions) (*kubernetes.Clientset, error) {
 	return clientset, nil
 }
 
-func (k *K8s) CustomPodDelete(k8sOptions K8sOptions, k8sPodOptions K8sPodOptions) ([]byte, error) {
+func (k *K8s) getClientCtx(options K8sOptions) (*kubernetes.Clientset, context.Context, context.CancelFunc, error) {
 
 	clientset := k.clientset
-	if clientset == nil || k8sOptions != k.options {
-		cs, err := newK8sClient(k8sOptions)
+	if clientset == nil || options != k.options {
+		cs, err := newK8sClient(options)
 		if err != nil {
-			return nil, err
+			return nil, nil, nil, err
 		}
 		clientset = cs
 	}
 
-	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(k8sOptions.Timeout)*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(options.Timeout)*time.Second)
+	return clientset, ctx, cancel, nil
+}
+
+func (k *K8s) CustomResourceDelete(options K8sOptions, deleteOptions K8sResourceDeleteOptions) ([]byte, error) {
+
+	clientset, ctx, cancel, err := k.getClientCtx(options)
+	if err != nil {
+		return nil, err
+	}
 	defer cancel()
 
-	opts := metav1.DeleteOptions{}
+	policy := metav1.DeletePropagationBackground
+	opts := metav1.DeleteOptions{
+		PropagationPolicy: &policy,
+	}
 
-	rClient := clientset.CoreV1().RESTClient()
+	var rClient rest.Interface
+
+	switch deleteOptions.Kind {
+	case K8sResourceDeployments:
+		rClient = clientset.AppsV1().RESTClient()
+	default:
+		rClient = clientset.CoreV1().RESTClient()
+	}
+
 	r := rClient.Delete().
-		UseProtobufAsDefaultIfPreferred(true).
-		NamespaceIfScoped(k8sPodOptions.Namespace, true).
-		Resource("pod").
-		Name(k8sPodOptions.Name).
+		UseProtobufAsDefaultIfPreferred(false).
+		NamespaceIfScoped(deleteOptions.Namespace, !utils.IsEmpty(deleteOptions.Namespace)).
+		Resource(deleteOptions.Kind).
+		Name(deleteOptions.Name).
 		Body(&opts).
 		Do(ctx)
 
 	return r.Raw()
 }
 
-func (k *K8s) PodDelete(options K8sPodOptions) ([]byte, error) {
-	return k.CustomPodDelete(k.options, options)
+func (k *K8s) ResourceDelete(options K8sResourceDeleteOptions) ([]byte, error) {
+	return k.CustomResourceDelete(k.options, options)
+}
+
+func (k *K8s) CustomResourceScale(options K8sOptions, scaleOptions K8sResourceScaleOptions) ([]byte, error) {
+
+	clientset, ctx, cancel, err := k.getClientCtx(options)
+	if err != nil {
+		return nil, err
+	}
+	defer cancel()
+
+	getOpts := metav1.GetOptions{}
+	rClient := clientset.AppsV1().RESTClient()
+
+	rGet := rClient.Get().
+		UseProtobufAsDefaultIfPreferred(false).
+		NamespaceIfScoped(scaleOptions.Namespace, !utils.IsEmpty(scaleOptions.Namespace)).
+		Resource(scaleOptions.Kind).
+		Name(scaleOptions.Name).
+		VersionedParams(&getOpts, scheme.ParameterCodec).
+		Do(ctx)
+
+	err = rGet.Error()
+	if err != nil {
+		return rGet.Raw()
+	}
+
+	var currentReplicas int32
+	desiredReplicasInt32 := int32(scaleOptions.Replicas)
+
+	var obj interface{}
+
+	switch scaleOptions.Kind {
+	case K8sResourceDeployments:
+
+		var d appsv1.Deployment
+		err = rGet.Into(&d)
+		if err != nil {
+			return nil, err
+		}
+		currentReplicas = *d.Spec.Replicas
+		d.Spec.Replicas = &desiredReplicasInt32
+		obj = &d
+
+	case K8sResourceReplicaSets:
+
+		var rs appsv1.ReplicaSet
+		err = rGet.Into(&rs)
+		if err != nil {
+			return nil, err
+		}
+		currentReplicas = *rs.Spec.Replicas
+		rs.Spec.Replicas = &desiredReplicasInt32
+		obj = &rs
+
+	case K8sResourceStatefulSets:
+
+		var ss appsv1.StatefulSet
+		err = rGet.Into(&ss)
+		if err != nil {
+			return nil, err
+		}
+
+		currentReplicas = *ss.Spec.Replicas
+		ss.Spec.Replicas = &desiredReplicasInt32
+		obj = &ss
+
+	default:
+		return nil, fmt.Errorf("unsupported kind for scaling: %s", scaleOptions.Kind)
+	}
+
+	if currentReplicas == desiredReplicasInt32 {
+		return nil, fmt.Errorf("current replicas are equal to desired replicas")
+	}
+
+	updateOpts := metav1.UpdateOptions{}
+	rPut := rClient.Put().
+		UseProtobufAsDefaultIfPreferred(false).
+		NamespaceIfScoped(scaleOptions.Namespace, !utils.IsEmpty(scaleOptions.Namespace)).
+		Resource(scaleOptions.Kind).
+		Name(scaleOptions.Name).
+		VersionedParams(&updateOpts, scheme.ParameterCodec).
+		Body(obj).
+		Do(ctx)
+
+	return rPut.Raw()
+}
+
+func (k *K8s) ResourceScale(options K8sResourceScaleOptions) ([]byte, error) {
+	return k.CustomResourceScale(k.options, options)
 }
 
 func NewK8s(options K8sOptions, logger common.Logger) *K8s {
